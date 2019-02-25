d018a50367a71c58b847e94ff95ce6d32a4b4d80
postrequest <postrequest@protonmail.com>
2019-02-26 02:35:08 +1100

    Clean up

--- gitupdater.c
+++ gitupdater.c
@@ -8,37 +8,28 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-void update_repo(const char *dirname)
-{
-    chdir(dirname);
-    system("git pull");
-}
-
 void isgit(const char *dirname)
 {
-    DIR *dirp;
-    if (dirp = opendir(dirname), dirp != 0) {
+    DIR *dirp = opendir(dirname);
+    if (dirp != 0) {
         struct dirent *dp;
         while (dp = readdir(dirp), dp != 0) {
-            char *path;
-            if (path =
-                malloc(strlen(dirname) + 1 + strlen(dp->d_name) + 1),
-                path != 0) {
+            char *path = malloc(strlen(dirname) + 1 + strlen(dp->d_name) + 1);
+            if (path) {
                 sprintf(path, "%s/%s", dirname, dp->d_name);
-
                 struct stat st;
-                if (strcmp(dp->d_name, ".git") == 0) {
-                    if (lstat(path, &st) != -1) {
+                if (!strcmp(dp->d_name, ".git")) {
+                    if (!lstat(path, &st)) {
                         if (S_ISDIR(st.st_mode)) {
                             pid_t spawn_for_update = fork();
                             if (spawn_for_update == -1) {
                                 perror("fork");
                                 exit(EXIT_FAILURE);
                             }
-                            if (spawn_for_update == 0) {
-                                update_repo(dirname);
+                            if (!spawn_for_update) {
+                                chdir(dirname);
+                                system("git pull");
                                 exit(EXIT_SUCCESS);
-                                wait(NULL);
                             }
                         }
                     }
@@ -56,26 +47,28 @@ int main(void)
     if (dirp = opendir("."), dirp != 0) {
         struct dirent *dp;
         while (dp = readdir(dirp), dp != 0) {
-            char *path;
-            if (path =
-                malloc(strlen(".") + 1 + strlen(dp->d_name) + 1),
-                path != 0) {
-                sprintf(path, "%s/%s", ".", dp->d_name);
-
+            char *path = malloc(2 + strlen(dp->d_name) + 1);
+            if (path) {
+                sprintf(path, "./%s", dp->d_name);
+                if (!strcmp(dp->d_name, ".") && !strcmp(dp->d_name, "..")) {
+                    continue;
+                }
                 struct stat st;
-                if (strcmp(dp->d_name, ".") != 0
-                    && strcmp(dp->d_name, "..") != 0) {
-                    if (lstat(path, &st) != -1) {
-                        if (S_ISDIR(st.st_mode)) {
-                            isgit(path);
-                        }
+                if (!lstat(path, &st)) {
+                    if (S_ISDIR(st.st_mode)) {
+                        isgit(path);
                     }
                 }
-
                 free(path);
             }
         }
         closedir(dirp);
+    }
+    while(1) {
+        wait(NULL);
+        if (errno == ECHILD) {
+            break;
+        }
     }
 
     return 0;
